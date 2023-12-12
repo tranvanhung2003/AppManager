636c6789a6c2d1a6388fe1f63a229bdddf89b092
Van Han Tran <tranvanhan29042007@gmail.com>
2023-12-12 10:44:10 +0700

    lan 2

--- app/src/main/java/com/example/appbanthuoc/activity/GioHangActivity.java
+++ app/src/main/java/com/example/appbanthuoc/activity/GioHangActivity.java
@@ -12,9 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appbanthuoc.R;
 import com.example.appbanthuoc.adapter.GioHangAdapter;
-import com.example.appbanthuoc.model.GioHang;
-
-import java.util.List;
+import com.example.appbanthuoc.utils.Utils;
 
 public class GioHangActivity extends AppCompatActivity {
     TextView giohangtrong, tongtien;
@@ -22,7 +20,6 @@ public class GioHangActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Button btnmuahang;
     GioHangAdapter adapter;
-    List<GioHang> gioHangList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +42,12 @@ public class GioHangActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        if (Utils.manggiohang.size() == 0) {
+            giohangtrong.setVisibility(View.VISIBLE);
+        } else {
+            adapter = new GioHangAdapter(getApplicationContext(), Utils.manggiohang);
+            recyclerView.setAdapter(adapter);
+        }
     }
 
     private void initView() {
